85a03864e7fa61ab12006bef4b55567e64e4a863
Scott Forman <sjforman@gmail.com>
2014-04-29 13:04:37 -0400

    trying again to get images to appear when published - case sensitivity?

--- js/site.js
+++ js/site.js
@@ -1,21 +1,21 @@
 var images = [
-'BBB_Lehigh_4.jpg',
-'BBB_Lehigh_1.jpg',
-'BBB_Lehigh_2.jpg',
-'BBB_Lehigh_3.jpg',
-'BBB_NoMA_1.jpg',
-'BBB_NoMA_2.jpg',
-'BBB_NMAH_FFandE_1.jpg',
-'BBB_NMAH_FFandE_2.jpg',
-'BBB_FourMileRun_1.jpg',
-'BBB_FourMileRun_2.jpg',
-'BBB_FourMileRun_3.jpg',
-'BBB_FourMileRun_4.jpg',
-'BBB_FL_NewRiverMasterPlan_1.jpg',
-'BBB_FL_NewRiverMasterPlan_2.jpg',
-'BBB_FL_NewRiverMasterPlan_3.jpg',
-'BBB_FL_ArtsDistrict_PublicMeeting.jpg',
-'CB_SeametreySchool.jpg'
+'BBB_Lehigh_4.JPG',
+'BBB_Lehigh_1.JPG',
+'BBB_Lehigh_2.JPG',
+'BBB_Lehigh_3.JPG',
+'BBB_NoMA_1.JPG',
+'BBB_NoMA_2.JPG',
+'BBB_NMAH_FFandE_1.JPG',
+'BBB_NMAH_FFandE_2.JPG',
+'BBB_FourMileRun_1.JPG',
+'BBB_FourMileRun_2.JPG',
+'BBB_FourMileRun_3.JPG',
+'BBB_FourMileRun_4.JPG',
+'BBB_FL_NewRiverMasterPlan_1.JPG',
+'BBB_FL_NewRiverMasterPlan_2.JPG',
+'BBB_FL_NewRiverMasterPlan_3.JPG',
+'BBB_FL_ArtsDistrict_PublicMeeting.JPG',
+'CB_SeametreySchool.JPG'
 ];
 
 var NUM_IMG = images.length;
