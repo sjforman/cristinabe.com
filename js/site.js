d2ced642885113e14162b78b7166fea677fff5ac
Scott Forman <sjforman@gmail.com>
2014-05-01 18:23:56 -0400

    Added index page and header pages with text and thumbnails for each section.

--- js/site.js
+++ js/site.js
@@ -1,36 +1,11 @@
-var images = [
-'BBB_Lehigh_4.JPG',
-'BBB_Lehigh_1.JPG',
-'BBB_Lehigh_2.JPG',
-'BBB_Lehigh_3.JPG',
-'BBB_NoMA_1.JPG',
-'BBB_NoMA_2.JPG',
-'BBB_NMAH_FFandE_1.JPG',
-'BBB_NMAH_FFandE_2.JPG',
-'BBB_FourMileRun_1.JPG',
-'BBB_FourMileRun_2.JPG',
-'BBB_FourMileRun_3.JPG',
-'BBB_FourMileRun_4.JPG',
-'BBB_FL_NewRiverMasterPlan_1.JPG',
-'BBB_FL_NewRiverMasterPlan_2.JPG',
-'BBB_FL_NewRiverMasterPlan_3.JPG',
-'BBB_FL_ArtsDistrict_PublicMeeting.JPG',
-'CB_SeametreySchool.JPG'
-];
-
-var NUM_IMG = images.length;
-
 $( document ).ready(function( ) {
 
   $('#fullpage').fullpage({
-        slidesColor : ['#006363', "white", '#006363','#006363','#006363','#006363','#006363','#006363','#006363'],
-});
-
-  for (var i = 0; i < NUM_IMG; i++) {
-    $("#images").append($("<img>",
-    {
-      src: "./slides/" + images[i],
-      alt: images[i]
-    }));
-  }
+          resize : false,
+          slidesColor : ['#006363', "white", '#006363','#006363','#006363','#006363','#006363','#006363','#006363'],
+          anchors : ['Home', 'Index', 'WTC', 'Lehigh', 'NoMA', 'NMAH', 'FourMile', 'NewRiver', 'Seametrey'],
+          scrollingSpeed : 300,
+          navigation: true, 
+          slidesNavigation: true
+  });
 });
